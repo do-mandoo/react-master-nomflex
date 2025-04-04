83a30f1a665f9750a2115423e619062691d63c6c
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2025-04-04 20:02:05 +0900

    라이브러리 설치(버전 확인)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme.ts';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
